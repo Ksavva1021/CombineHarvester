95564769a3be8f625e4baf488e2b82a232c4ab95
Klitos <kleitos.savva.97@gmail.com>
2022-12-10 13:22:17 +0000

    removed older files

--- Combine4tau/shapes/all/jobs/tau_decay_mode_2_ettt_inclusive_ff_123.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /vols/cms/gu18/4tau_v3/CMSSW_10_2_19//src/UserCode/ICHiggsTauTau/Analysis/4tau
-source /vols/grid/cms/setup.sh
-export SCRAM_ARCH=slc6_amd64_gcc481
-eval 'scramv1 runtime -sh'
-source /vols/cms/gu18/4tau_v3/CMSSW_10_2_19//src/UserCode/ICHiggsTauTau/Analysis/4tau/scripts/setup_libs.sh
-ulimit -c 0
-python /vols/cms/gu18/4tau_v3/CMSSW_10_2_19/src/UserCode/ICHiggsTauTau/Analysis/4tau/scripts/combined_year_4tauPlot.py --outputfolder=/vols/cms/gu18/4tau_v3/CMSSW_10_2_19/src/UserCode/ICHiggsTauTau/Analysis/4tau/0512/ettt --options="--folder=/vols/cms/gu18/Offline/output/4tau/2411_ff_v2 --no_signal --charges_non_zero --ff_from=123 --under_legend='FF_{1} x FF_{2} x FF_{3}' --method=2 --var='tau_decay_mode_2[0,1,2,3,4,5,6,7,8,9,10,11,12]' --vsjets=loose --ratio_range=0,2"  --channel=ettt --cat=inclusive --run_datacards --extra_name=tau_decay_mode_2_ff_123
